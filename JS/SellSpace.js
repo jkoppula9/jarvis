81a79053922f260e9e6c1f3f83611d784b639f44
jkoppula9 <koppulajayasree3@gmail.com>
2021-11-02 20:47:38 -0400

    Added images and search logic

--- JS/SellSpace.js
+++ JS/SellSpace.js
@@ -1,12 +1,12 @@
 
 var sellspaceitems=[];
 
-var item1={"itemname":"lamp"};
-var item2={"itemname":"T.V"};
-var item3={"itemname":"Cupboard"};
-var item4={"itemname":"Sofa"};
-var item5={"itemname":"Table"};
-var item6={"itemname":"Shoes"};
+var item1={"itemname":"lamp","imageurl":"./images/lamp.jpg"};
+var item2={"itemname":"Car","imageurl":"./images/car.jpg"};
+var item3={"itemname":"Chair","imageurl":"./images/chair.jpg"};
+var item4={"itemname":"Table","imageurl":"./images/table.jpg"};
+var item5={"itemname":"Laptop","imageurl":"./images/lappy.jpg"};
+var item6={"itemname":"Shoes","imageurl":"./images/shoes.jpg"};
 sellspaceitems.push(item1);
 sellspaceitems.push(item2);
 sellspaceitems.push(item3);
@@ -16,13 +16,13 @@ sellspaceitems.push(item6);
 
 function search(){
 
-var inputtext = document.getElementById("searchtxt").value;
+var inputtext = document.getElementById("searchtxt").value.toLowerCase();
   console.log(inputtext);
   console.log(sellspaceitems.length);
   var filterditems=[];
   for(var i=0;i<sellspaceitems.length;i++){
 
-    if(sellspaceitems[i].itemname.includes(inputtext)){
+    if(sellspaceitems[i].itemname.toLowerCase().includes(inputtext)){
       filterditems.push(sellspaceitems[i]);
     }
 
@@ -42,7 +42,7 @@ function setview(viewitems){
     console.log(viewitems[i].itemname);
     
     objTo.innerHTML+='<div class="card" style="width:300px">'+
-      '<img class="card-img-top" src="./images/lamp.jpg"  alt="Card image" style="width:100%">'+
+      '<img class="card-img-top" height="200" width="150" '+'src="'+viewitems[i].imageurl+'"'+'alt="Card image" style="width:100%">'+
       '<div class="card-body">'+
         '<h4 class="card-title">'+viewitems[i].itemname+'</h4>'+
        '<p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>'+
